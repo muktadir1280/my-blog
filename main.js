6c18b3b91a619a85c1870f411240de08171791fb
muktadir1280 <muktadir1280@gmail.com>
2026-02-15 15:51:13 +0600

    button issues fix

--- main.js
+++ main.js
@@ -337,7 +337,7 @@ const app = {
                     ${
                       showReadMore
                         ? `
-                        <button class="text-sm font-semibold text-gray-900 hover:underline inline-flex items-center gap-1">
+                        <button class="text-sm font-semibold text-blue-600 hover:underline inline-flex items-center gap-1">
                             Read More <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/></svg>
                         </button>
                     `
